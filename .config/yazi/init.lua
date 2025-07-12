588921978204fca24ed51788b7cbc71e90ceafc0
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2025-07-13 02:46:51 +0800

    yazi: cleanup & upgrade plugins

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,17 +1,2 @@
 require("git"):setup()
 require("starship"):setup()
-
--- bunny.yazi for bookmarks
-local home = os.getenv("HOME")
-require("bunny"):setup({
-  hops = {
-    { tag = "home", path = home, key = "h" },
-    { tag = "config", path = home.."/.config", key = "c" },
-    { tag = "app data", path = home.."/.local/share", key = "l" },
-    { tag = "downloads", path = home.."/Downloads", key = "d" },
-    { tag = "documents", path = home.."/Documents", key = "D" },
-    { tag = "music", path = home.."/Music", key = "m" },
-    { tag = "pictures", path = home.."/Pictures", key = "p" },
-    { tag = "data drive", path = "/data", key = "t" },
-  },
-})
