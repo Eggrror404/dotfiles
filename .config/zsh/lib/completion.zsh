fd524aa0096328a6429ab72d50ca6e1df76cb87e
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2025-07-13 02:13:00 +0800

    yazi, zsh: update config

--- .config/zsh/lib/completion.zsh
+++ .config/zsh/lib/completion.zsh
@@ -10,4 +10,5 @@ bashcompinit
 # app completions
 if type register-python-argcomplete &>/dev/null; then
   eval "$(register-python-argcomplete pipx)"
+  eval "$(register-python-argcomplete pmbootstrap)"
 fi
