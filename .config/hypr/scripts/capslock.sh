dfb012c0f82a55071c733988f89a9ca23a7d7044
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2025-07-17 23:05:29 +0800

    hypr: update animations, exec, qs script

--- .config/hypr/scripts/capslock.sh
+++ .config/hypr/scripts/capslock.sh
@@ -1,7 +1,7 @@
 capslock=$(cat /sys/class/leds/*capslock*/brightness | head -n 1)
 
 if [ "$capslock" = "1" ]; then
-    quickshell ipc call osd openIcon "CapsLock On" "caps-lock-symbolic"
+    quickshell ipc call osd openIcon "CapsLock On" "keyboard_capslock_badge"
 else
-    quickshell ipc call osd openIcon "CapsLock Off" "caps-lock-off-symbolic"
+    quickshell ipc call osd openIcon "CapsLock Off" "keyboard_capslock"
 fi
